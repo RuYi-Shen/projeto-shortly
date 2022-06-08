997c1a783b35edca1a86fddb95313c06869e9d12
Ru Yi Shen <ruyishen2012@gmail.com>
2022-06-08 13:42:04 -0300

    feat : get url by id implemented - status ok

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -17,3 +17,17 @@ export async function generateShortUrl(req, res) {
     res.status(500).send(error);
   }
 }
+
+export async function getUrl(req, res) {
+  const id = req.params.id;
+  try {
+    const url = await db.query(`SELECT urls.id, urls."shortUrl", urls.url FROM urls WHERE id = $1`, [id]);
+    if (url.rows.length === 0) {
+      return res.status(404).send("Url not found");
+    }
+    res.status(200).json(url.rows[0]);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+}
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { generateShortUrl } from '../controllers/urlController.js';
+import { generateShortUrl, getUrl } from '../controllers/urlController.js';
 
 //import { validateToken } from '../middlewares/urlMiddleware.js';
 import { validateToken, validateUrl } from '../middlewares/schemasMiddleware.js';
@@ -7,8 +7,8 @@ import { validateToken, validateUrl } from '../middlewares/schemasMiddleware.js'
 const urlRouter = Router();
 
 urlRouter.post("/shorten", validateToken, validateUrl, generateShortUrl);
+urlRouter.get("/:id", getUrl);
 /* urlRouter.get("/open/:shortUrl", validateSignIn, validateUser, signIn);
-urlRouter.get("/:id", validateSignIn, validateUser, signIn);
 urlRouter.delete("/:id", validateSignIn, validateUser, signIn); */
 
 export default urlRouter;
