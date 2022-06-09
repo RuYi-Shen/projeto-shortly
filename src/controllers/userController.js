e915e5ecf2cec0ed32b3a543c493cf64098f081d
Ru Yi Shen <ruyishen2012@gmail.com>
2022-06-09 12:47:53 -0300

    feat : get users url data by id implemented - status ok

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,14 +3,23 @@ import db from "../db.js";
 export async function getUser(req, res) {
   const id = req.params.id;
   try {
-    const url = await db.query(
-      `SELECT urls.id, urls."shortUrl", urls.url FROM urls WHERE id = $1`,
+    const user = await db.query(
+      `SELECT users.id, users.name FROM users WHERE users.id = $1`,
       [id]
     );
-    if (url.rows.length === 0) {
-      return res.status(404).send("Url not found");
+    if (user.rows.length === 0) {
+      return res.status(404).send("User not found");
     }
-    res.status(200).json(url.rows[0]);
+    const urls = await db.query(
+        `SELECT urls.id, urls."shortUrl", urls.url, urls."visitCount" FROM urls WHERE urls."userId" = $1`,
+        [id]
+    );
+    user.rows[0].visitCount = 0;
+    urls.rows.forEach(url => {
+        user.rows[0].visitCount += url.visitCount;
+    });
+    user.rows[0].shortenedUrls = urls.rows;
+    res.json(user.rows[0]);
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
